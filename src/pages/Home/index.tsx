b278550f18b35f97bf0c9c85dbcd0788f5cce24c
Kevin-Sarges <kevinsarges08@gmail.com>
2022-04-14 18:39:00 -0300

    Finalizando pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,76 @@
+import { useState, useEffect } from "react";
+import { MdAddShoppingCart } from "react-icons/md";
+import { toast } from "react-toastify";
+
+import { api } from "../../services/api";
+import { formatPrice } from "../../utils/format";
+import { useCart } from "../../context/useCart";
+
 import { Container } from "./styles";
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  score: number;
+  image: string;
+}
+
+interface ProductFormatted extends Product {
+  priceFormatted: string;
+}
+
+interface CartItemAmount {
+  [key: number]: number;
+}
+
 export function Home() {
+  const { addPropduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+
+  const cartItemAmount = cart.reduce((sumAmount, product) => {
+    const newSumAmount = { ...sumAmount };
+    newSumAmount[product.id] = product.amount;
+
+    return newSumAmount;
+  }, {} as CartItemAmount);
+
+  useEffect(() => {
+    async function loadProducts() {
+      try {
+        const { data } = await api.get("/products");
+
+        setProducts(data);
+      } catch (error) {
+        toast.error("Erro ao lista produtos !!");
+      }
+    }
+
+    loadProducts();
+  }, []);
+
+  async function handleAddProduct(id: number) {
+    addPropduct(id);
+  }
+
   return (
     <Container>
-      <p>Home</p>
+      {products.map((product) => (
+        <li key={product.id}>
+          <img src={product.image} alt="Produto" />
+          <strong>{product.name}</strong>
+          <span>{formatPrice(product.price)}</span>
+
+          <button type="button" onClick={() => handleAddProduct(product.id)}>
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {cartItemAmount[product.id] || 0}
+            </div>
+
+            <span>Adicionar ao Carrinho</span>
+          </button>
+        </li>
+      ))}
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,71 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  margin: 0;
+  max-width: 1020px;
+  margin: 0 auto;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  list-style: none;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+
+    img {
+      align-self: center;
+      max-width: 250px;
+    }
+
+    > strong {
+      font-size: 16px;
+      line-height: 20px;
+      color: #333;
+      margin-top: 5px;
+    }
+
+    > span {
+      font-size: 21px;
+      font-weight: bold;
+      margin: 5px 0 20px;
+    }
+
+    button {
+      margin-top: auto;
+      display: flex;
+      align-items: center;
+      background: #191920;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      overflow: hidden;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background-color: #3284ff;
+      }
+
+      div {
+        display: flex;
+        align-items: center;
+        padding: 12px;
+        background: rgba(0, 0, 0, 0.1);
+        border-right: 1px solid #3284ff;
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+
+      span {
+        flex: 1;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
+  }
 `;
