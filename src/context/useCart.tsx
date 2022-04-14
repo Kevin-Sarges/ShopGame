cf3c6f2081d42f9657946591747a90fea99a2d3c
Kevin-Sarges <kevinsarges08@gmail.com>
2022-04-14 09:03:14 -0300

    Finalizando contexto de carrinho de compras

--- src/context/useCart.tsx
+++ src/context/useCart.tsx
@@ -54,9 +54,97 @@ export function CartProvider({ children }: CartProvideProps) {
     }
   }, [cart, cartPreviousValue]);
 
+  async function addPropduct(productId: number) {
+    try {
+      const updateCart = [...cart];
+      const productExists = updateCart.find(
+        (product) => product.id === productId
+      );
+
+      const stock = await api.get(`/stock/${productId}`);
+      const stockAmount = stock.data.amount;
+      const currentAmount = productExists ? productExists.amount : 0;
+
+      const amount = currentAmount + 1;
+
+      if (amount > stockAmount) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      if (productExists) {
+        productExists.amount = amount;
+      } else {
+        const product = await api.get(`/products/${productId}`);
+
+        const newProduct = {
+          ...product.data,
+          amount: 1,
+        };
+
+        updateCart.push(newProduct);
+      }
+
+      setCart(updateCart);
+    } catch (error) {
+      toast.error("Erro na adição do produto");
+    }
+  }
+
+  function removeProduct(productId: number) {
+    try {
+      const updateCart = [...cart];
+      const productIndex = updateCart.findIndex(
+        (product) => product.id === productId
+      );
+
+      if (productIndex >= 0) {
+        updateCart.splice(productIndex, 1);
+        setCart(updateCart);
+      } else {
+        throw Error();
+      }
+    } catch (error) {
+      toast.error("Erro na remoção do produto");
+    }
+  }
+
+  async function updateProductAmount({
+    productId,
+    amount,
+  }: UpdateProductAmount) {
+    try {
+      if (amount <= 0) {
+        return;
+      }
+
+      const stock = await api.get(`/stock/${productId}`);
+      const stockAmount = stock.data.amount;
+
+      if (amount > stockAmount) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      const updateCart = [...cart];
+      const productExists = updateCart.find(
+        (product) => product.id === productId
+      );
+
+      if (productExists) {
+        productExists.amount = amount;
+        setCart(updateCart);
+      } else {
+        throw Error();
+      }
+    } catch (error) {
+      toast.error("Erro na alteração de quantidade do produto");
+    }
+  }
+
   return (
     <CartContext.Provider
-      value={{ cart, addProduct, removeProduct, updateProductAmount }}
+      value={{ cart, addPropduct, removeProduct, updateProductAmount }}
     >
       {children}
     </CartContext.Provider>
