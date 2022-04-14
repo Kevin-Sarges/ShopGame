e3c1963156713556118cb3cc30077ee54f2a8905
Kevin-Sarges <kevinsarges08@gmail.com>
2022-04-14 17:38:11 -0300

    Criando rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
+import { RoutesApp } from "./routes";
+import { Header } from "./components/Header";
+import { CartProvider } from "./context/useCart";
 import GlobalStyles from "./styles/global";
 
 function App() {
   return (
     <BrowserRouter>
-      <h1>Teste</h1>
-      <GlobalStyles />
-      <ToastContainer autoClose={3000} />
+      <CartProvider>
+        <Header />
+        <RoutesApp />
+        <GlobalStyles />
+        <ToastContainer autoClose={3000} />
+      </CartProvider>
     </BrowserRouter>
   );
 }
